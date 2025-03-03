bdc566e65c3dab4a663985fbe87a11f31b168042
netow2005 <netodnet@gmail.com>
2025-03-03 16:25:03 -0300

    Bugfix CORS

--- src/main/java/projeto/neto/projeto_api_rest/ProjetoApiRestApplication.java
+++ src/main/java/projeto/neto/projeto_api_rest/ProjetoApiRestApplication.java
@@ -1,8 +1,11 @@
 package projeto.neto.projeto_api_rest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server URL")})
 @SpringBootApplication
 public class ProjetoApiRestApplication {
 
